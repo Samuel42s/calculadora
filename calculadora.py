7be6fff53ec4373afe57ebe26d84d7660a2510ad
Samuel Augusto <augustosamuel759@gmail.com>
2024-02-28 08:52:56 -0300

    commit inicial

---
+++ calculadora.py
@@ -0,0 +1,68 @@
+print('----------- CALCALADORA -----------')
+print('            BEM VINDO(a)'           )
+print('-----------------------------------')
+print('                                   ')
+
+while True:
+    print('                                   ')
+    print('------------ OPERAÇÕES ------------')
+    print('             OPÇÕES                ')
+    print('             1 - Soma (+)          ')
+    print('             2 - Subtração (-)     ')
+    print('             3 - Multiplicação (*) ')
+    print('             4 - Divisão (/)       ')
+    print('             5 - Potencia (**)     ')
+    print('             6 - Sair              ')
+    print('-----------------------------------')
+    print('                                   ')
+
+    opcao = int(input('escolha uma opção: '))
+
+    if opcao == 1:
+        numero1 = float(input('digite o primeiro numero: '))
+        numero2 = float(input('digite o segundo numero: '))
+        resultado = numero1 + numero2
+        print(f'{numero1} + {numero2} = {resultado}')
+
+    elif opcao == 2:
+        numero1 = float(input('digite o primeiro numero: '))
+        numero2 = float(input('digite o segundo numero: '))
+        resultado = numero1 - numero2
+        print(f'{numero1} - {numero2} = {resultado}')
+
+    elif opcao == 3:
+        numero1 = float(input('digite o primeiro numero: '))
+        numero2 = float(input('digite o segundo numero: '))
+        resultado = numero1 * numero2
+        print(f'{numero1} X {numero2} = {resultado}')
+
+    elif opcao == 4:
+        numero1 = float(input('digite o primeiro numero: '))
+        numero2 = float(input('digite o segundo numero: '))
+        resultado = numero1 / numero2
+        print(f'{numero1} / {numero2} = {resultado}')
+
+    elif opcao == 5:
+        numero1 = float(input('digite o primeiro numero: '))
+        numero2 = float(input('digite o segundo numero: '))
+        resultado = numero1 ** numero2
+        print(resultado)
+
+    elif opcao == 6:
+        print('                                   ')
+        print('----------- Já vai? :( ------------')
+        print('----------- até a proxima ---------')
+        break
+
+    
+    print('                                   ')
+    print('----------- continuar? ------------')
+    print('            sim ou nao?            ')
+    print('-----------------------------------')
+    print('                                   ')
+    continuar = input('s / n: ')
+    if continuar != 's' and continuar != 'S':
+        print('                                   ')
+        print('----------- Já vai? :( ------------')
+        print('----------- até a proxima ---------')
+        break
